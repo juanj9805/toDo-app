e0676d644b3bc92f3a527b0d8d3f6b903a1ef83a
juan jose barrera <juanjoseb9805@gmail.com>
2025-07-26 14:56:16 -0500

    ok

--- app.js
+++ app.js
@@ -1,51 +1,31 @@
-const btnDeleteEl = document.querySelector(".delete");
-const btnCreateEl = document.querySelector(".add");
-const inputTaskEL = document.querySelector(".form");
-const divTaskEl = document.querySelector(".list");
+const btnAdd = document.querySelector(".create");
+const ul = document.querySelector(".group");
+const taskInput = document.querySelector(".taskInput");
+const form = document.querySelector("form");
 
 const tasks = [];
 
-btnCreateEl.addEventListener("click", function () {
-  //   inputTaskEL.value = "";
-  const value = inputTaskEL.value;
-  const html = `
-          <div class="input-group mb-3">
-
-  <li class="list-group-item d-flex justify-content-between">
-  ${value}
-
-  <button type="button" id="add" class="col add btn btn-success">
-  <input type="checkbox" name="" id="">
-  </button>
-  <button class="delete btn btn-danger btn-sm">Delete</button>
-
-  </li>
-  </div>
-  `;
-
-  tasks.push(value);
-  console.log(typeof value);
-
-  //   console.log("hi");
-  console.log(tasks);
-
-  divTaskEl.insertAdjacentHTML("afterbegin", html);
-});
-
-// btnDeleteEl?.addEventListener("click", function () {
-divTaskEl.addEventListener("click", function (event) {
-  // Check if the clicked element has the 'delete' class
-  if (event.target.classList.contains("delete")) {
-    console.log("Delete clicked!");
-
-    // Remove the task
-    event.target.closest("li").remove();
+btnAdd.addEventListener("click", function (e) {
+  const input = taskInput.value;
+  if (input !== "") {
+    const html = `
+    <div class="task">
+    <h3>${taskInput.value}</h3>
+    <button class="delete">X</button>
+    </div>
+    `;
+    ul.insertAdjacentHTML("afterbegin", html);
   }
-  tasks.pop();
+  tasks.push(input);
+  console.log(tasks);
+  // input.innerHTML = "";
 });
 
-inputTaskEL.addEventListener("click", function () {
-  inputTaskEL.computedStyleMap.textDecortation;
+addEventListener("click", function (e) {
+  if (e.target.classList.contains("delete")) {
+    e.target.closest(".task")?.remove();
+    console.log("hi");
+    tasks.pop();
+  }
+  console.log(tasks);
 });
-
-// console.log("juan".le);
